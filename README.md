53b57aaccc744926e1a3c7dc208f064b0f3f3cc2
Arlodotexe <arlodotexe@outlook.com>
2018-04-18 02:19:17 -0500

    Update readme, add package.json, wrap more say.js features

--- README.md
+++ README.md
@@ -6,17 +6,29 @@ There is not currently an NPM module for async-sayjs. For now, simply download `
 
 # Usage
 
-This wrapper is brand new and _stupidly_ simple. It doesn't carry over all of the functionality of say.js, yet. Here's what it can do:
+NOTE: In it's current form, async-say will fail to execute asyncronously when an error occurs (such as giving it an invalid TTS Voice to use). This will probably be fixed in the future.
 
 ``` Javascript
 
 const say = require('./async-say.js');
 
-// Speak something asynchronously
+// Speak something asynchronously (wait for each request to finish before moving on)
 say.speak('The quick brown fox');
 
+// Speak something synchronously (Say it right now, even if something is already being said)
+say.now('Jumped over the lazy dog');
+
 // Supply a string as the second parameter to change the TTS voice
-say.speak('Jumped over the lazy dog', 'Microsoft Eva Mobile');
+say.speak('As it ran through the woods', 'Microsoft David Desktop');
 
+// Speak something, but slowly
+say.speak('Lorem ipsum dolor sit amet', 'Microsoft Zira Mobile', 0.5);
 
+// Speak something, then fire a callback
+say.speak('Lorem ipsum dolor sit amet', 'Microsoft David Desktop', 0.5, (err) => {
+    console.log('Done');
+});
+
+// Stop anything currently being spoken (Queued text will continue after that)
+say.stop(callback);
 ```
--- async-say.js
+++ async-say.js
@@ -1,39 +1,37 @@
 const say = require('say');
 
-   function speak() {
-    let fn = function(){};
-    let speechQ = [];
-    fn.speak = function(phrase, TTSVoice) {
-        // Speak asynchronously, waiting for each TTS request to finish before speaking the next one
+function speak() {
+    let fn = function() { };
+    var speechQ = [];
+    fn.speak = function(phrase, TTSVoice, speed, callback) {
         speaking = {
-            now: null,
-            set: function(bool) {
-                this.now = bool;
-            }
+            now: null
         }
         function trySpeech() {
             speechQ.push(phrase);
-            if(speaking.now === false || speechQ.length == 1) {
-                speaking.now = true;	
-                say.speak(speechQ.slice(-1)[0], TTSVoice, 1.0, (err) => {
+
+            if (speaking.now === false || speechQ.length == 1) {
+                speaking.now = true;
+                say.speak(speechQ.slice(-1)[0], TTSVoice, speed, (err) => {
                     speaking.now = false;
                     speechQ.pop();
-                    if (err) {
-                        console.log(err);
-                    }
+                    if(callback) callback();
                 });
             } else {
-                speaking.now = true;
                 setTimeout(function() {
                     trySpeech();
-                }, 100);
+                }, 250);
             }
         }
         trySpeech();
-    }	
+    }
     fn.now = function(phrase, TTSVoice) {
-        // Speak immediately, talking over any other current TTS outputs 
         say.speak(phrase, TTSVoice);
+    }
+    fn.stop = function(callback) {
+        say.stop(() => {
+            if (callback) callback();
+        });
     }
     return fn;
 }
